36b896de098770bef8caf3b782f7e51750522030
chunyukuo88 <alexgochenour@gmail.com>
2026-03-02 17:00:15 -0500

    feat: links

--- src/components/project-cards/ProjectCards.jsx
+++ src/components/project-cards/ProjectCards.jsx
@@ -1,4 +1,4 @@
-import './project-cards.css';
+import styles from './project-cards.module.css';
 import { useEffect, useRef } from "preact/hooks";
 import { Card1, Card2, Card3, Card4, Card5 } from "./components/";
 
@@ -65,22 +65,32 @@ export default function ProjectCards() {
 
     return (
         <>
-            <h3 id="projects">
+            <h3 id={styles.projects}>
                 <span aria-hidden="true">Projects</span>
             </h3>
-            <div className="project-cards">
-                <h5 className="project-cards-welcome">Note: Links to all projects can be found at the bottom of the page.</h5>
+            <div className={styles["project-cards"]}>
+                <h5 className={styles["project-cards-welcome"]}>Note: Links to all projects can be found at the bottom of the page.</h5>
                 {cardObjects.map((card, i) => (
                     <div
                         key={i}
-                        className="card-wrapper"
+                        className={styles["card-wrapper"]}
                         ref={el => cardsRef.current[i] = el}
                     >
                         <card.component/>
                     </div>
                 ))}
-                <div className="hidden-card">
-                    Check back for the latest live projects!
+                <div className={styles["hidden-card"]}>
+                    <p>Links:</p>
+                    <div className={styles["links-table"]}>
+                        <a href="http://woobler-svelte.s3-website-us-east-1.amazonaws.com/" target="_blank">Woobler's Other House</a>
+                        <a href="https://github.com/chunyukuo88/woobler-sveltekit" target="_blank">Code</a>
+                        <div>This site</div>
+                        <a href="https://github.com/chunyukuo88/astro-portfolio" target="_blank">Code</a>
+                        <div>Workouts API</div>
+                        <a href="https://github.com/chunyukuo88/workoutsV2" target="_blank">Code</a>
+                        <a href="https://www.npmjs.com/package/gochenour" target="_blank">The gochenour CLI</a>
+                        <a href="https://github.com/chunyukuo88/gochenour" target="_blank">Code</a>
+                    </div>
                 </div>
             </div>
         </>
